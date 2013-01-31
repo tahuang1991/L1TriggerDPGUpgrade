2083f8b00117031b3820b649b3e46793da952a4b
Lindsey Gray <lindsey.gray@gmail.com>
2013-01-31 04:03:44 -0600

    add in reflex def files

---
+++ src/classes.h
@@ -0,0 +1,26 @@
+#include "L1Trigger/L1IntegratedMuonTrigger/interface/TriggerPrimitive.h"
+#include "L1Trigger/L1IntegratedMuonTrigger/interface/TriggerPrimitiveFwd.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Common/interface/RefToBase.h"
+
+namespace {
+  using namespace L1ITMu;
+  struct dictionary {
+    TriggerPrimitive::RPCData rpcD;
+    TriggerPrimitive::CSCData cscD;
+    TriggerPrimitive::DTData  dtD;
+    
+    edm::Wrapper<TriggerPrimitive::RPCData> wrpcD;
+    edm::Wrapper<TriggerPrimitive::CSCData> wcscD;
+    edm::Wrapper<TriggerPrimitive::DTData>  wdtD;
+
+    TriggerPrimitive trigPrim;
+    edm::Wrapper<TriggerPrimitive> wtrigPrim;
+    TriggerPrimitiveCollection vtrigPrim;
+    edm::Wrapper<TriggerPrimitiveCollection> wvtrigPrim;
+
+    TriggerPrimitiveRef rtrigPrim;
+    
+    TriggerPrimitivePtr ptrigPrim;    
+  };
+}
