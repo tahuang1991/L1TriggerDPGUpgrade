e9dd4f48a2a6de5545201873fabcbe4a8b722433
Lindsey Gray <lindsey.gray@gmail.com>
2013-02-01 08:02:00 -0600

    RPC Collector in

---
+++ interface/RPCCollector.h
@@ -0,0 +1,31 @@
+#ifndef __L1ITMU_RPCCOLLECTOR_H__
+#define __L1ITMU_RPCCOLLECTOR_H__
+// 
+// Class: L1ITMu::RPCCollector
+//
+// Info: Processes RPC digis into ITMu trigger primitives. 
+//       Positional information is not assigned here.
+//
+// Author: L. Gray (FNAL)
+//
+#include <vector>
+#include "L1Trigger/L1IntegratedMuonTrigger/interface/SubsystemCollector.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+class L1MuDTChambPhDigi;
+class L1MuDTChambThDigi;
+
+namespace L1ITMu {
+  
+  class RPCCollector: public SubsystemCollector {
+  public:
+    RPCCollector(const edm::ParameterSet&);
+    ~RPCCollector() {}
+
+    virtual void extractPrimitives(const edm::Event&, const edm::EventSetup&, 
+				   std::vector<TriggerPrimitive>&) const;
+  private:    
+  };
+}
+
+#endif
---
+++ plugins/RPCCollector.cc
@@ -0,0 +1,39 @@
+#include "L1Trigger/L1IntegratedMuonTrigger/interface/RPCCollector.h"
+#include "DataFormats/RPCDigi/interface/RPCDigiL1Link.h"
+#include "DataFormats/MuonDetId/interface/RPCDetId.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+
+using namespace L1ITMu;
+
+RPCCollector::RPCCollector( const edm::ParameterSet& ps ):
+  SubsystemCollector(ps) {
+}
+
+void RPCCollector::extractPrimitives(const edm::Event& ev, 
+				    const edm::EventSetup& es, 
+				    std::vector<TriggerPrimitive>& out) const {
+  edm::Handle<std::vector<RPCDigiL1Link> > rpcDigis;  
+  ev.getByLabel(_src,rpcDigis);
+  
+  auto digi = rpcDigis->begin();
+  auto end  = rpcDigis->end();
+
+  for( ; digi != end; ++digi ) {
+    for( unsigned layer = 1; layer <= digi->nlayer(); ++layer ){
+      const RPCDetId id(digi->rawdetId(layer));
+      if( ! digi->empty(layer) ) {
+	out.push_back(TriggerPrimitive(id,
+				       digi->strip(layer),
+				       layer,
+				       digi->bx(layer))
+		      );
+      }
+    }
+  }
+}
+
+#include "L1Trigger/L1IntegratedMuonTrigger/interface/SubsystemCollectorFactory.h"
+DEFINE_EDM_PLUGIN( SubsystemCollectorFactory, RPCCollector, "RPCCollector");
