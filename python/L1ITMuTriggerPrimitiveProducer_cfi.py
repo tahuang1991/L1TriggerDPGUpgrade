356a64f6f461d215153fd7eefc4041c6a56dc00b
Lindsey Gray <lindsey.gray@gmail.com>
2013-02-04 11:18:36 -0600

    fix configuration

--- python/L1ITMuTriggerPrimitiveProducer_cfi.py
+++ python/L1ITMuTriggerPrimitiveProducer_cfi.py
@@ -2,9 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 L1ITMuTriggerPrimitives = cms.EDProducer(
     'L1ITMuTriggerPrimitiveProducer',
-    DT   = cms.PSet( src = cms.InputTag('dtTriggerPrimitiveDigis') ),
-    RPCb = cms.PSet( src = cms.InputTag('simRpcTriggerDigis', 'RPCb') ),
-    CSC  = cms.PSet( src = cms.InputTag('cscTriggerPrimitiveDigis',
+    DT   = cms.PSet( collectorType = cms.string('DTCollector'),
+                     src = cms.InputTag('dtTriggerPrimitiveDigis') ),
+    RPCb = cms.PSet( collectorType = cms.string('RPCCollector'),
+                     src = cms.InputTag('simRpcTriggerDigis', 'RPCb') ),
+    CSC  = cms.PSet( collectorType = cms.string('CSCCollector'),
+                     src = cms.InputTag('cscTriggerPrimitiveDigis',
                                         'MPCSORTED') ),
-    RPCf = cms.PSet( src = cms.InputTag('simRpcTriggerDigis', 'RPCf') )    
+    RPCf = cms.PSet( collectorType = cms.string('RPCCollector'),
+                     src = cms.InputTag('simRpcTriggerDigis', 'RPCf') )    
     )
