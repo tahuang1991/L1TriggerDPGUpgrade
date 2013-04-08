c4e416d142ffb3e5d3eb4301b5cf8310ef405cb4
Lindsey Gray <lindsey.gray@gmail.com>
2013-04-08 06:37:25 -0500

    skeleton of pt assignment and refinement

---
+++ L1TMuon/interface/PtRefinementUnit.h
@@ -0,0 +1,36 @@
+#ifndef __L1TMUON_PTREFINEMENTUNIT_H__
+#define __L1TMUON_PTREFINEMENTUNIT_H__
+// 
+// Class: L1TMuon::PtRefinementUnit
+//
+// Info: This is a base class for any algorithm that takes a found track
+//       with assigned pT and applies a refinement to that estimate.
+//
+// Author: L. Gray (FNAL)
+//
+#include <vector>
+#include "L1TriggerDPGUpgrade/DataFormats/interface/L1TMuonInternalTrackFwd.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+namespace edm {
+  class ParameterSet;
+  class Event;
+  class EventSetup;
+}
+
+namespace L1TMuon {
+  
+  class PtRefinementUnit {
+  public:
+    PtRefinementUnit(const edm::ParameterSet&);
+    virtual ~PtRefinementUnit() {}
+
+    virtual InternalTrackCollection 
+      refinePt(const edm::EventSetup&, 
+	       const InternalTrackCollection&) const = 0;
+  protected:
+    std::string _name;
+  };
+}
+
+#endif
