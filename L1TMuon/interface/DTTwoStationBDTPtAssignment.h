d7d2db461e9d6078c647821a38f49f09ebe346c4
Lindsey Gray <lindsey.gray@gmail.com>
2013-05-01 15:39:59 -0500

    add in DT luts and root files

---
+++ L1TMuon/interface/DTTwoStationBDTPtAssignment.h
@@ -0,0 +1,30 @@
+#ifndef __L1TMUON_DTTWOSTATIONBDTPTASSIGNMENT_H__
+#define __L1TMUON_DTTWOSTATIONBDTPTASSIGNMENT_H__
+// 
+// Class: L1TMuon::DTTwoStationBDTPtAssignment
+//
+// Info: An implementation of the two station BDT pt assignment 
+//       used in the TDR studies.
+//       Note: this only works for DT tracks!
+//
+// Author: L. Gray (FNAL), B. Scurlock (UF)
+//
+#include <vector>
+#include "L1TriggerDPGUpgrade/L1TMuon/interface/PtAssignmentUnit.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+namespace L1TMuon {
+  
+  class DTTwoStationBDTPPtAssignment: public PtAssignmentUnit {
+  public:
+    DTTwoStationBDTPPtAssignment(const edm::ParameterSet&);
+    ~DTTwoStationBDTPPtAssignment() {}
+
+    virtual void updateEventSetup(const edm::EventSetup&) {}
+    
+    virtual void assignPt(InternalTrack&) const {}
+  private:    
+  };
+}
+
+#endif
