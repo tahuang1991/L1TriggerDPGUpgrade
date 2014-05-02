1cdbb0b22e3e3d229cb9d2e6a3281d19a70a11d6
tahuang1991 <taohuang@physics.tamu.edu>
2014-05-02 13:24:00 -0500

    addd simple scripts to get plots

---
+++ L1TMuon/scripts/TrackFinderEffVsPtme1.py
@@ -0,0 +1,160 @@
+from ROOT import *
+from PlotHelper import *
+global input_dir 
+global file_name1 
+global file_name2
+
+#_______________________________________________________________
+def ThreeStubsWith():
+
+	c1 = TCanvas("c","c",800,600)
+	c1.SetGridx()
+	c1.SetGridy()
+	c1.SetTickx()
+	c1.SetTicky()
+
+	b1 = TH1F("b1","b1",50,0,50)
+	b1.GetYaxis().SetRangeUser(0.60,1)
+	b1.GetYaxis().SetTitleOffset(1.2)
+	b1.GetYaxis().SetNdivisions(520)
+	b1.GetYaxis().SetTitle("TrackFinder Reco efficiency")
+	b1.GetXaxis().SetTitle("#eta of simulated muon track")
+	b1.SetTitle(" "*64 + "CMS Simulation Preliminary")
+	b1.SetStats(0)
+
+	dir1 = getRootDirectory(input_dir, file_name1)
+	dir2 = getRootDirectory(input_dir, file_name2)
+
+	hist_name = "L1CSCTrack_3Stubs_pt_me1"
+
+	h1 = getH(dir1, "%s"%hist_name)
+	h2 = getH(dir2, "%s"%hist_name)
+
+
+	h1.SetLineColor(ROOT.kRed)
+	h1.SetLineWidth(2)
+	h2.SetLineColor(ROOT.kBlue)
+	h2.SetLineWidth(2)
+
+	b1.Draw()
+	h1.Draw("same")
+	h2.Draw("same")
+
+
+	legend = TLegend(0.23,0.16,0.52,0.44,"","brNDC")
+	legend.SetFillColor(ROOT.kWhite)
+        legend.SetFillStyle(0)
+	legend.SetHeader("PU0")
+	legend.AddEntry(h1,"current+GE11","l")
+	legend.AddEntry(h2,"current+GE11+GE21","l")
+	legend.Draw("same")
+
+	c1.SaveAs("%s_PU0.pdf"%hist_name)
+	c1.SaveAs("%s_PU0.png"%hist_name)
+
+#_______________________________________________________________
+def ThreeStubsWithME1():
+
+	c1 = TCanvas("c","c",800,600)
+	c1.SetGridx()
+	c1.SetGridy()
+	c1.SetTickx()
+	c1.SetTicky()
+
+	b1 = TH1F("b1","b1",50,0,50)
+	b1.GetYaxis().SetRangeUser(0.60,1)
+	b1.GetYaxis().SetTitleOffset(1.2)
+	b1.GetYaxis().SetNdivisions(520)
+	b1.GetYaxis().SetTitle("TrackFinder Reco efficiency")
+	b1.GetXaxis().SetTitle("#eta of simulated muon track")
+	b1.SetTitle(" "*64 + "CMS Simulation Preliminary")
+	b1.SetStats(0)
+
+	dir1 = getRootDirectory(input_dir, file_name1)
+	dir2 = getRootDirectory(input_dir, file_name2)
+
+	hist_name = "L1CSCTrack_3StubsME1_pt_me1"
+
+	h1 = getH(dir1, "%s"%hist_name)
+	h2 = getH(dir2, "%s"%hist_name)
+
+
+	h1.SetLineColor(ROOT.kRed)
+	h1.SetLineWidth(2)
+	h2.SetLineColor(ROOT.kBlue)
+	h2.SetLineWidth(2)
+
+	b1.Draw()
+	h1.Draw("same")
+	h2.Draw("same")
+
+
+	legend = TLegend(0.23,0.16,0.52,0.44,"","brNDC")
+	legend.SetFillColor(ROOT.kWhite)
+        legend.SetFillStyle(0)
+	legend.SetHeader("PU0")
+	legend.AddEntry(h1,"current+GE11","l")
+	legend.AddEntry(h2,"current+GE11+GE21","l")
+	legend.Draw("same")
+
+	c1.SaveAs("%s_PU0.pdf"%hist_name)
+	c1.SaveAs("%s_PU0.png"%hist_name)
+
+#_______________________________________________________________
+def ThreeStubsWithME1PtCut():
+
+	c1 = TCanvas("c","c",800,600)
+	c1.SetGridx()
+	c1.SetGridy()
+	c1.SetTickx()
+	c1.SetTicky()
+
+	b1 = TH1F("b1","b1",50,0,50)
+	b1.GetYaxis().SetRangeUser(0.60,1)
+	b1.GetYaxis().SetTitleOffset(1.2)
+	b1.GetYaxis().SetNdivisions(520)
+	b1.GetYaxis().SetTitle("TrackFinder Reco efficiency")
+	b1.GetXaxis().SetTitle("#eta of simulated muon track")
+	b1.SetTitle(" "*64 + "CMS Simulation Preliminary")
+	b1.SetStats(0)
+
+	dir1 = getRootDirectory(input_dir, file_name1)
+	dir2 = getRootDirectory(input_dir, file_name2)
+
+	hist_name = "L1CSCTrack_3Stubs20ME1_pt_me1"
+
+	h1 = getH(dir1, "%s"%hist_name)
+	h2 = getH(dir2, "%s"%hist_name)
+
+
+	h1.SetLineColor(ROOT.kRed)
+	h1.SetLineWidth(2)
+	h2.SetLineColor(ROOT.kBlue)
+	h2.SetLineWidth(2)
+
+	b1.Draw()
+	h1.Draw("same")
+	h2.Draw("same")
+
+
+	legend = TLegend(0.23,0.16,0.52,0.44,"","brNDC")
+	legend.SetFillColor(ROOT.kWhite)
+        legend.SetFillStyle(0)
+	legend.SetHeader("PU0")
+	legend.AddEntry(h1,"current+GE11","l")
+	legend.AddEntry(h2,"current+GE11+GE21","l")
+	legend.Draw("same")
+
+
+	c1.SaveAs("%s_PU0.pdf"%hist_name)
+	c1.SaveAs("%s_PU0.png"%hist_name)
+
+#_______________________________________________________________________________
+if __name__ == "__main__":
+	
+	input_dir = "files/"
+	file_name1 = "histo_out_L1muon2023GE11.root"
+	file_name2 = "histo_out_L1muon2023GE11GE21.root"
+	ThreeStubsWith()
+	ThreeStubsWithME1()
+	ThreeStubsWithME1PtCut()
