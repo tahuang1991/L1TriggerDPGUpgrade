647b51bc889a104c08484fcf057e4cdd46672999
Jason <jason.lee@cern.ch>
2014-05-07 20:47:38 +0900

    trigger rate analyser fix

--- L1TMuon/plugins/L1TTriggerRate.cc
+++ L1TMuon/plugins/L1TTriggerRate.cc
@@ -73,47 +73,20 @@ private:
   double max_pt;
   double min_aEta;
   double max_aEta;
+  float ntotalEvents = 0.0;
+  const float ptscale[33] = { 
+    -1.,   0.0,   1.5,   2.0,   2.5,   3.0,   3.5,   4.0,
+    4.5,   5.0,   6.0,   7.0,   8.0,  10.0,  12.0,  14.0,  
+    16.0,  18.0,  20.0,  25.0,  30.0,  35.0,  40.0,  45.0, 
+    50.0,  60.0,  70.0,  80.0,  90.0, 100.0, 120.0, 140.0, 1.E6 };
+  const float xptscale[32] = { 
+    0.0,   1.5,   2.0,   2.5,   3.0,   3.5,   4.0,
+    4.5,   5.0,   6.0,   7.0,   8.0,  10.0,  12.0,  14.0,  
+    16.0,  18.0,  20.0,  25.0,  30.0,  35.0,  40.0,  45.0, 
+    50.0,  60.0,  70.0,  80.0,  90.0, 100.0, 120.0, 140.0, 200.0};
+  unsigned maxPTbins = 32;
 
-  TH1F* h_GEMDPhi;
-  TH1F* h_nStation;
-  TH1F* h_nStationTF;
-  TH1F* h_nStationTFloweta;
-  TH1F* h_nStationTFlowetalowphi;  
-
-  TH1F* h_StubQuality;
-  TH1F* h_StubQualityGEM;
-
-  TH1F* h_TFStubQuality;
-  TH1F* h_TFStubQualityGEM;
-
-  TH1F* h_TFnStubinTrack;
-  TH1F* h_TFnStubinTrackGEM;
-
-  TH2F* h_TFnStubvsPTAdd;
-  TH2F* h_TFnStubvsPTAddGEM;
-
-  TH2F* h_TFnStubvsQualityPacked;
-  TH2F* h_TFnStubvsQualityPackedGEM;
-
-  TH2F* h_TFPTpackedvsPTAdd;
-  TH2F* h_TFPTpackedvsPTAddGEM;
-
-  TH2F* h_TFPTpackedvsdPhi12;
-  TH2F* h_TFPTpackedvsdPhi23;
-
-  TH2F* h_TFPTAddvsdPhi12;
-  TH2F* h_TFPTAddvsdPhi23;
-
-  TH2F* h_TFPTpackedvsdPhi12GEM;
-  TH2F* h_TFPTpackedvsdPhi23GEM;
-  TH2F* h_TFPTpackedvsGEMdPhi;
-
-  TH1F* h_noTFnStub;
-  TH1F* h_noTFnStubGEM;
-  TH1F* h_noTFQ3nStub;
-  TH1F* h_noTFQ3nStubGEM;
-
-  enum etabins{eta_all, eta_me1, eta_me2, netabins};
+  enum etabins{eta_all, eta_me1, eta_me2, eta_me3, netabins};
   enum ptbins{pt_all, pt_20, nptbins};
   enum stubbins{stub_2, stub_3, nstubbins};
   enum MEbins{ME_all, ME_1, ME_2, nMEbins};
@@ -121,7 +94,7 @@ private:
   TH1F* h_L1CSCTrack_eta[netabins][nptbins][nstubbins][nMEbins];
   TH1F* h_L1CSCTrack_phi[netabins][nptbins][nstubbins][nMEbins];
 
-  TH1F* h_TFnStubinTrack_phihole;
+  TH1F* h_nStation;
 
 };
 //
@@ -157,47 +130,27 @@ L1TTriggerRate::~L1TTriggerRate()
 // ------------ method called to for each event  ------------
 void L1TTriggerRate::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-  ///////////////////
-  //Setup Stuff//////
-  ///////////////////
-  //  edm::RefProd<CSCTrackCollection> csctfProd = ev.getRefBeforePut<CSCTrackCollection>("input");  
-  const float ptscale[33] = { 
-    -1.,   0.0,   1.5,   2.0,   2.5,   3.0,   3.5,   4.0,
-    4.5,   5.0,   6.0,   7.0,   8.0,  10.0,  12.0,  14.0,  
-    16.0,  18.0,  20.0,  25.0,  30.0,  35.0,  40.0,  45.0, 
-    50.0,  60.0,  70.0,  80.0,  90.0, 100.0, 120.0, 140.0, 1.E6 };
-
   edm::Handle<L1CSCTrackCollection> l1csctracks;
   iEvent.getByLabel("simCsctfTrackDigis",l1csctracks);
   edm::Handle<TriggerPrimitiveCollection> trigPrims;
   iEvent.getByLabel("L1TMuonTriggerPrimitives",trigPrims);
   edm::Handle<CSCCorrelatedLCTDigiCollection> lcts;
   iEvent.getByLabel("simCscTriggerPrimitiveDigis","MPCSORTED", lcts);
-  
-  // all lcts
-  for(CSCCorrelatedLCTDigiCollection::DigiRangeIterator csc=lcts->begin(); csc!=lcts->end(); csc++){
-    if ((*csc).first.endcap() == 1){
-      h_nStation->Fill((*csc).first.station());
-    }
-    if ((*csc).first.endcap() == 2){
-      h_nStation->Fill(-((*csc).first.station()));
-    }
-  }
-
+  ++ntotalEvents;
   // lcts used in tracks
   L1CSCTrackCollection::const_iterator tmp_trk = l1csctracks->begin();
   for(; tmp_trk != l1csctracks->end(); tmp_trk++){
-    float GEMdPhi = -99;
+    //    float GEMdPhi = -99;
     float pt=0, eta=-9, phi=-9;
-    unsigned int quality_packed=0, rank=0, ptLUTAddress=0;
+    unsigned int quality_packed=0, rank=0;//ptLUTAddress=0;
     unsigned int pt_packed=0;
 
     auto l1track = tmp_trk->first;
     rank=l1track.rank();
-    ptLUTAddress = l1track.ptLUTAddress();
+    // ptLUTAddress = l1track.ptLUTAddress();
     l1track.decodeRank(rank,pt_packed,quality_packed); //get the pt and gaulity packed
     pt = ptscale[pt_packed];
-	
+    
     unsigned int sector = l1track.sector();// get sector
     edm::ESHandle< L1MuTriggerScales > scales;//get structures for scales (phi and eta
     iSetup.get< L1MuTriggerScalesRcd >().get(scales); // get scales from EventSetup
@@ -208,58 +161,63 @@ void L1TTriggerRate::analyze(const edm::Event& iEvent, const edm::EventSetup& iS
     phi = ts->getPhiScale()->getLowEdge( gbl_phi&0xff );
     unsigned eta_sign = (l1track.endcap() == 1 ? 0 : 1);
     eta = ts->getRegionalEtaScale(2)->getCenter( ((l1track.eta_packed()) | (eta_sign<<5)) & 0x3f );
+    if (phi > 3.14159265359)
+      phi = 3.14159265359 - phi;
+    int nstubs=0;
+    bool hasME1=false;
+    bool hasME2=false;
+    CSCCorrelatedLCTDigiCollection::DigiRangeIterator csc=tmp_trk->second.begin();
+    for(; csc!=tmp_trk->second.end(); csc++){
+      if ((*csc).first.station()==1) hasME1 = true;
+      if ((*csc).first.station()==2) hasME2 = true;
+      nstubs++;
+    }
 
     for (int nstubbin = 0; nstubbin < nstubbins; nstubbin++){
-      for (int netabin = 0; netabin < netabins; netabin++){
-	for (int nptbin = 0; nptbin < nptbins; nptbin++){
-	  for (int nMEbin = 0; nMEbin < nMEbins; nMEbin++){
-	    h_L1CSCTrack_pt[netabin][nptbin][nstubbin][nMEbin]->Fill(pt);
-	    h_L1CSCTrack_eta[netabin][nptbin][nstubbin][nMEbin]->Fill(eta);
-	    h_L1CSCTrack_phi[netabin][nptbin][nstubbin][nMEbin]->Fill(phi);
+      if (((nstubbin == stub_2) && (nstubs > 1)) ||
+	  ((nstubbin == stub_3) && (nstubs > 2))){
+
+	for (int nMEbin = 0; nMEbin < nMEbins; nMEbin++){
+	  if ((nMEbin == ME_all) ||
+	      ((nMEbin == ME_1) && (hasME1)) ||
+	      ((nMEbin == ME_2) && (hasME2))){
+
+	    for (int netabin = 0; netabin < netabins; netabin++){
+	      if ((netabin == eta_all) ||
+		  ((netabin == eta_me3) && (eta > 1.64 && eta < 2.14)) ||
+		  ((netabin == eta_me1) && (eta > 1.6 && eta < 2.1)) ||
+		  ((netabin == eta_me2) && (eta > 2.1 && eta < 2.4))){
+   
+		for (unsigned nptbin = 0; nptbin < maxPTbins; nptbin++){
+		  if (pt >= ptscale[nptbin]){
+		    h_L1CSCTrack_pt[netabin][0][nstubbin][nMEbin]->Fill(ptscale[nptbin]);
+		  }
+		}
+		for (unsigned nptbin = 0; nptbin < nptbins; nptbin++){
+		  if ((nptbin == pt_all) ||
+		      ((nptbin == pt_20) && (pt >= 20))){
+		    h_L1CSCTrack_eta[netabin][nptbin][nstubbin][nMEbin]->Fill(eta);
+		    h_L1CSCTrack_phi[netabin][nptbin][nstubbin][nMEbin]->Fill(phi);
+		  }
+		}
+	      }
+	    }
 	  }
 	}
       }
     }
-    int nstubs = 0;
-    bool hasGEM = false;
-    for(CSCCorrelatedLCTDigiCollection::DigiRangeIterator csc=tmp_trk->second.begin(); csc!=tmp_trk->second.end(); csc++){
-      auto lctdigi = (*csc).second.first;
-      nstubs++;
-      h_GEMDPhi->Fill((*csc).second.first->getGEMDPhi());
+  }
 
-      if ((*csc).first.endcap() == 1){
-	h_nStationTF->Fill((*csc).first.station());
-      }
-      if ((*csc).first.endcap() == 2){
-	h_nStationTF->Fill(-((*csc).first.station()));
-      }
-      if ((*csc).second.first->getGEMDPhi() > -99){
-	hasGEM = true;
-	GEMdPhi = (*csc).second.first->getGEMDPhi();
-	h_TFStubQualityGEM->Fill(lctdigi->getQuality());
-      }
-      h_TFStubQuality->Fill(lctdigi->getQuality());
+  // all lcts
+  for(CSCCorrelatedLCTDigiCollection::DigiRangeIterator csc=lcts->begin(); csc!=lcts->end(); csc++){
+    if ((*csc).first.endcap() == 1){
+      h_nStation->Fill((*csc).first.station());
     }
-
-    h_TFnStubinTrack->Fill(nstubs);
-    h_TFnStubvsPTAdd->Fill(nstubs,ptLUTAddress);
-    h_TFnStubvsQualityPacked->Fill(nstubs,quality_packed);
-    h_TFPTpackedvsPTAdd->Fill(pt_packed,ptLUTAddress);
-    h_TFPTpackedvsdPhi12->Fill(pt_packed,l1track.deltaPhi12());
-    h_TFPTpackedvsdPhi23->Fill(pt_packed,l1track.deltaPhi23());
-    h_TFPTAddvsdPhi12->Fill(ptLUTAddress,l1track.deltaPhi12());
-    h_TFPTAddvsdPhi23->Fill(ptLUTAddress,l1track.deltaPhi23());
-
-    if (hasGEM){
-      h_TFnStubinTrackGEM->Fill(nstubs);
-      h_TFnStubvsPTAddGEM->Fill(nstubs,ptLUTAddress);
-      h_TFnStubvsQualityPackedGEM->Fill(nstubs,quality_packed);
-      h_TFPTpackedvsPTAddGEM->Fill(pt_packed,ptLUTAddress);
-      h_TFPTpackedvsdPhi12GEM->Fill(pt_packed,l1track.deltaPhi12());
-      h_TFPTpackedvsdPhi23GEM->Fill(pt_packed,l1track.deltaPhi23());
-      h_TFPTpackedvsGEMdPhi->Fill(pt_packed,GEMdPhi);
+    if ((*csc).first.endcap() == 2){
+      h_nStation->Fill(-((*csc).first.station()));
     }
   }
+
 }
 
 
@@ -267,7 +225,7 @@ void L1TTriggerRate::analyze(const edm::Event& iEvent, const edm::EventSetup& iS
 void L1TTriggerRate::beginJob()
 {
 
-  TString etabinsName[] = {"", "eta1", "eta2"};
+  TString etabinsName[] = {"", "eta1", "eta2", "eta3"};
   TString ptbinsName[] = {"", "pt20"};
   TString stubbinsName[] = {"stub2", "stub3"};
   TString MEbinsName[] = {"", "hasME1", "hasME2"};
@@ -276,136 +234,27 @@ void L1TTriggerRate::beginJob()
       for (int nptbin = 0; nptbin < nptbins; nptbin++){
 	for (int nMEbin = 0; nMEbin < nMEbins; nMEbin++){
 	  h_L1CSCTrack_pt[netabin][nptbin][nstubbin][nMEbin]
-	    = fs->make<TH1F>("L1cscTrack_"+stubbinsName[nstubbin]+ptbinsName[nptbin]+MEbinsName[nMEbin]+etabinsName[netabin]+"_pt", "", 20, 0, 50);
-	  h_L1CSCTrack_pt[netabin][nptbin][nstubbin][nMEbin]->GetXaxis()->SetTitle("simulated muon p_{T} [GeV]");
-	  h_L1CSCTrack_pt[netabin][nptbin][nstubbin][nMEbin]->GetYaxis()->SetTitle("Efficiency");
+	    = fs->make<TH1F>("L1cscTrack_"+stubbinsName[nstubbin]+ptbinsName[nptbin]+MEbinsName[nMEbin]+etabinsName[netabin]+"_pt", "", 31, xptscale);
+	  h_L1CSCTrack_pt[netabin][nptbin][nstubbin][nMEbin]->GetXaxis()->SetTitle("L1 muon p_{T} [GeV]");
+	  h_L1CSCTrack_pt[netabin][nptbin][nstubbin][nMEbin]->GetYaxis()->SetTitle("Rate [kHz]");
 
 	  h_L1CSCTrack_eta[netabin][nptbin][nstubbin][nMEbin]
 	    = fs->make<TH1F>("L1cscTrack_"+stubbinsName[nstubbin]+ptbinsName[nptbin]+MEbinsName[nMEbin]+etabinsName[netabin]+"_eta", "", 50,1.5,2.5);
-	  h_L1CSCTrack_eta[netabin][nptbin][nstubbin][nMEbin]->GetXaxis()->SetTitle("simulated muon #eta");
-	  h_L1CSCTrack_eta[netabin][nptbin][nstubbin][nMEbin]->GetYaxis()->SetTitle("Efficiency");
+	  h_L1CSCTrack_eta[netabin][nptbin][nstubbin][nMEbin]->GetXaxis()->SetTitle("muon #eta");
+	  h_L1CSCTrack_eta[netabin][nptbin][nstubbin][nMEbin]->GetYaxis()->SetTitle("Rate [kHz]");
 
 	  h_L1CSCTrack_phi[netabin][nptbin][nstubbin][nMEbin]
 	    = fs->make<TH1F>("L1cscTrack_"+stubbinsName[nstubbin]+ptbinsName[nptbin]+MEbinsName[nMEbin]+etabinsName[netabin]+"_phi", "", 70,-3.5,3.5);
-	  h_L1CSCTrack_phi[netabin][nptbin][nstubbin][nMEbin]->GetXaxis()->SetTitle("simulated muon #phi");
-	  h_L1CSCTrack_phi[netabin][nptbin][nstubbin][nMEbin]->GetYaxis()->SetTitle("Efficiency");
+	  h_L1CSCTrack_phi[netabin][nptbin][nstubbin][nMEbin]->GetXaxis()->SetTitle(" muon #phi");
+	  h_L1CSCTrack_phi[netabin][nptbin][nstubbin][nMEbin]->GetYaxis()->SetTitle("Rate [kHz]");
 	}
       }
     }
   }
-  h_StubQuality=fs->make<TH1F>("StubQuality","Stub Quality",20,0,20);
-  h_StubQuality->GetXaxis()->SetTitle("Stub Quality");
-  h_StubQuality->GetYaxis()->SetTitle("Counts");
 
-  h_StubQualityGEM=fs->make<TH1F>("StubQualityGEM","Stub Quality",20,0,20);
-  h_StubQualityGEM->GetXaxis()->SetTitle("Stub Quality");
-  h_StubQualityGEM->GetYaxis()->SetTitle("Counts");
-
-  h_TFStubQuality=fs->make<TH1F>("TFStubQuality","Stub Quality",20,0,20);
-  h_TFStubQuality->GetXaxis()->SetTitle("Stub Quality");
-  h_TFStubQuality->GetYaxis()->SetTitle("Counts");
-
-  h_TFStubQualityGEM=fs->make<TH1F>("TFStubQualityGEM","Stub Quality",20,0,20);
-  h_TFStubQualityGEM->GetXaxis()->SetTitle("Stub Quality");
-  h_TFStubQualityGEM->GetYaxis()->SetTitle("Counts");
-
-  h_TFnStubinTrack=fs->make<TH1F>("TFnStubinTrack","N Stubs in Track",6,0,6);
-  h_TFnStubinTrack->GetXaxis()->SetTitle("N Stubs in Track");
-  h_TFnStubinTrack->GetYaxis()->SetTitle("Counts");
-
-  h_TFnStubinTrack_phihole=fs->make<TH1F>("TFnStubinTrack_phihole","N Stubs in Track",6,0,6);
-  h_TFnStubinTrack_phihole->GetXaxis()->SetTitle("N Stubs in Track");
-  h_TFnStubinTrack_phihole->GetYaxis()->SetTitle("Counts");
-
-  h_TFnStubinTrackGEM=fs->make<TH1F>("TFnStubinTrackGEM","N Stubs in Track",6,0,6);
-  h_TFnStubinTrackGEM->GetXaxis()->SetTitle("N Stubs in Track");
-  h_TFnStubinTrackGEM->GetYaxis()->SetTitle("Counts");
-
-  h_TFnStubvsPTAdd=fs->make<TH2F>("TFnStubvsPTAdd","N Stubs in Track Vs Pt address",6,0,6,500,0,4500000);
-  h_TFnStubvsPTAdd->GetXaxis()->SetTitle("N Stubs in Track");
-  h_TFnStubvsPTAdd->GetYaxis()->SetTitle("Pt address");
-
-  h_TFnStubvsPTAddGEM=fs->make<TH2F>("TFnStubvsPTAddGEM","N Stubs in Track Vs Pt address",6,0,6,500,0,4500000);
-  h_TFnStubvsPTAddGEM->GetXaxis()->SetTitle("N Stubs in Track");
-  h_TFnStubvsPTAddGEM->GetYaxis()->SetTitle("Pt address");
-
-  h_TFnStubvsQualityPacked=fs->make<TH2F>("TFnStubvsQualityPacked","N Stubs in Track vs Quality",6,0,6,5,0,5);
-  h_TFnStubvsQualityPacked->GetXaxis()->SetTitle("N Stubs in Track");
-  h_TFnStubvsQualityPacked->GetYaxis()->SetTitle("Quality");
-
-  h_TFnStubvsQualityPackedGEM=fs->make<TH2F>("TFnStubvsQualityPackedGEM","N Stubs in Track Vs Quality",6,0,6,5,0,5);
-  h_TFnStubvsQualityPackedGEM->GetXaxis()->SetTitle("N Stubs in Track");
-  h_TFnStubvsQualityPackedGEM->GetYaxis()->SetTitle("Quality");
-
-  h_TFPTpackedvsPTAdd=fs->make<TH2F>("TFPTpackedvsPTAdd","Pt packed Vs Pt address",40,0,40,500,0,4500000);
-  h_TFPTpackedvsPTAdd->GetXaxis()->SetTitle("Pt packed");
-  h_TFPTpackedvsPTAdd->GetYaxis()->SetTitle("Pt address");
-
-  h_TFPTpackedvsPTAddGEM=fs->make<TH2F>("TFPTpackedvsPTAddGEM","Pt packed Vs Pt address",40,0,40,500,0,4500000);
-  h_TFPTpackedvsPTAddGEM->GetXaxis()->SetTitle("Pt packed");
-  h_TFPTpackedvsPTAddGEM->GetYaxis()->SetTitle("Pt address");
-
-  h_TFPTpackedvsdPhi12=fs->make<TH2F>("TFPTpackedvsdPhi12","Pt packed Vs dPhi12",40,0,40,200,0,300);
-  h_TFPTpackedvsdPhi12->GetXaxis()->SetTitle("Pt packed");
-  h_TFPTpackedvsdPhi12->GetYaxis()->SetTitle("dPhi12");
-
-  h_TFPTpackedvsdPhi23=fs->make<TH2F>("TFPTpackedvsdPhi23","Pt packed Vs dPhi23",40,0,40,200,0,20);
-  h_TFPTpackedvsdPhi23->GetXaxis()->SetTitle("Pt packed");
-  h_TFPTpackedvsdPhi23->GetYaxis()->SetTitle("dPhi23");
-
-  h_TFPTAddvsdPhi12=fs->make<TH2F>("TFPTAddvsdPhi12","Pt Add Vs dPhi12",500,0,4500000,200,0,300);
-  h_TFPTAddvsdPhi12->GetXaxis()->SetTitle("Pt Add");
-  h_TFPTAddvsdPhi12->GetYaxis()->SetTitle("dPhi12");
-
-  h_TFPTAddvsdPhi23=fs->make<TH2F>("TFPTAddvsdPhi23","Pt Add Vs dPhi23",500,0,4500000,200,0,20);
-  h_TFPTAddvsdPhi23->GetXaxis()->SetTitle("Pt Add");
-  h_TFPTAddvsdPhi23->GetYaxis()->SetTitle("dPhi23");
-
-  h_TFPTpackedvsdPhi12GEM=fs->make<TH2F>("TFPTpackedvsdPhi12GEM","Pt packed Vs dPhi12",40,0,40,200,0,300);
-  h_TFPTpackedvsdPhi12GEM->GetXaxis()->SetTitle("Pt packed");
-  h_TFPTpackedvsdPhi12GEM->GetYaxis()->SetTitle("dPhi12");
-
-  h_TFPTpackedvsdPhi23GEM=fs->make<TH2F>("TFPTpackedvsdPhi23GEM","Pt packed Vs dPhi23",40,0,40,200,0,20);
-  h_TFPTpackedvsdPhi23GEM->GetXaxis()->SetTitle("Pt packed");
-  h_TFPTpackedvsdPhi23GEM->GetYaxis()->SetTitle("dPhi23");
-
-  h_TFPTpackedvsGEMdPhi=fs->make<TH2F>("TFPTpackedvsGEMdPhi","Pt packed Vs GEMdPhi",40,0,40,200,-0.5,0.5);
-  h_TFPTpackedvsGEMdPhi->GetXaxis()->SetTitle("Pt packed");
-  h_TFPTpackedvsGEMdPhi->GetYaxis()->SetTitle("GEMdPhi");
-
-  h_noTFnStub=fs->make<TH1F>("noTFnStub","no track, No. Stubs",6,0,6);
-  h_noTFnStub->GetXaxis()->SetTitle("No. Stubs");
-  h_noTFnStub->GetYaxis()->SetTitle("Counts");
-
-  h_noTFnStubGEM=fs->make<TH1F>("noTFnStubGEM","no track, No. Stubs",6,0,6);
-  h_noTFnStubGEM->GetXaxis()->SetTitle("No. Stubs");
-  h_noTFnStubGEM->GetYaxis()->SetTitle("Counts");
-
-  h_noTFQ3nStub=fs->make<TH1F>("noTFQ3nStub","no Q3 track, No. Stubs",6,0,6);
-  h_noTFQ3nStub->GetXaxis()->SetTitle("No. Stubs");
-  h_noTFQ3nStub->GetYaxis()->SetTitle("Counts");
-
-  h_noTFQ3nStubGEM=fs->make<TH1F>("noTFQ3nStubGEM","no Q3 track, No. Stubs",6,0,6);
-  h_noTFQ3nStubGEM->GetXaxis()->SetTitle("No. Stubs");
-  h_noTFQ3nStubGEM->GetYaxis()->SetTitle("Counts");
-
-  h_GEMDPhi=fs->make<TH1F>("GemDPhi","GEM-SCS dPhi in station 1",100,-0.1,0.1);
-  h_GEMDPhi->GetXaxis()->SetTitle("dPhi");
-  h_GEMDPhi->GetYaxis()->SetTitle("Counts");
-
-  h_nStationTF=fs->make<TH1F>("nStationTF","stations in track",11,-5,6);
-  h_nStationTF->GetXaxis()->SetTitle("Station number");
-  h_nStationTF->GetYaxis()->SetTitle("Counts");
-  h_nStationTFloweta=fs->make<TH1F>("nStationTFloweta","stations in track",11,-5,6);
-  h_nStationTFloweta->GetXaxis()->SetTitle("Station number");
-  h_nStationTFloweta->GetYaxis()->SetTitle("Counts");
-  h_nStationTFlowetalowphi=fs->make<TH1F>("nStationTFlowetalowphi","stations in track",11,-5,6);
-  h_nStationTFlowetalowphi->GetXaxis()->SetTitle("Station number");
-  h_nStationTFlowetalowphi->GetYaxis()->SetTitle("Counts");
-
-  h_nStation=fs->make<TH1F>("nStation","LCT stations",11,-5,6);
-  h_nStation->GetXaxis()->SetTitle("Station number");
-  h_nStation->GetYaxis()->SetTitle("Counts");
+  h_nStation=fs->make<TH1F>("nStation","",11,-5,6);
+  h_nStation->GetXaxis()->SetTitle("LCT stations");
+  h_nStation->GetYaxis()->SetTitle("Rate [kHz]");
 
 }
 
@@ -419,10 +268,16 @@ void L1TTriggerRate::endJob()
 	  h_L1CSCTrack_pt[netabin][nptbin][nstubbin][nMEbin]->Sumw2();
 	  h_L1CSCTrack_eta[netabin][nptbin][nstubbin][nMEbin]->Sumw2();
 	  h_L1CSCTrack_phi[netabin][nptbin][nstubbin][nMEbin]->Sumw2();
+
+	  h_L1CSCTrack_pt[netabin][nptbin][nstubbin][nMEbin]->Scale(40000./ntotalEvents/3.*0.795);
+	  h_L1CSCTrack_eta[netabin][nptbin][nstubbin][nMEbin]->Scale(40000./ntotalEvents/3.*0.795);
+	  h_L1CSCTrack_phi[netabin][nptbin][nstubbin][nMEbin]->Scale(40000./ntotalEvents/3.*0.795);
 	}
       }
     }
   }
+  h_nStation->Sumw2();
+  h_nStation->Scale(40000./ntotalEvents/3.*0.795);
 }
 
 //define this as a plug-in
